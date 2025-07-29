42aa9af29c2bf76d7accdbd0223dfc9b8eacd027
VihaanB <vihaan.bhaduri@gmail.com>
2025-07-28 18:34:58 -0700

    Changed app name in Github to Disypher.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 import 'dart:math';
 import 'dart:ui';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/foundation.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 
 import 'camera_screen.dart';
